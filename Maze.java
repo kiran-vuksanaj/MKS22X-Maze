14bd51166368bb25c5ec03fd87e1c872cf30128e
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-02-26 09:53:34 -0500

    code to print scanner, wrapper for file access

---
+++ Maze.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Scanner;
+public class Maze{
+  public static void main(String[] args){
+    Scanner sca = getScanner("Maze1.txt");
+    while(sca.hasNext()) System.out.println(sca.nextLine());
+  }
+  public static Scanner getScanner(String fileName){
+    return null;
+  }
+}

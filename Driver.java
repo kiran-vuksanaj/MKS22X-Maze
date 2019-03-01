e7a5f07eb28cdbe08298830efb0aae24f5c71e06
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-01 10:07:13 -0500

    improved driver, incorporating ethan's generation code

--- Driver.java
+++ Driver.java
@@ -2,21 +2,32 @@ import java.io.*;
 public class Driver{
     public static void main(String[]args){
       String filename = args[0];
+      boolean gen = indexOf(args,"gen") != -1;
+      int rDim = -1;
+      int cDim = -1;
+      if(gen){
+        rDim = Integer.parseInt(args[indexOf(args,"gen")+1]);
+        cDim = Integer.parseInt(args[indexOf(args,"gen")+2]);
+      }
+      boolean anim = indexOf(args,"anim") != -1;
       try{
-        Maze f;
-        f = new Maze(filename);//true animates the maze.
-
-        //f.setAnimate(true);
-        System.out.println(f.solve());
-        System.out.println(f);
-      }catch(FileNotFoundException e){
+        if(gen){
+          MazeGen g = new MazeGen(rDim,cDim);
+          g.generate();
+          g.writeToFile(filename);
+        }
+        Maze m = new Maze(filename);
+        if(anim) m.setAnimate(true);
+        System.out.println(m.solve());
+        System.out.println(m);
+      }catch(IOException e){
         System.out.println("Invalid filename: "+filename);
       }
     }
-    private static boolean contains(String[] L,String term){
-      for(String s : L){
-        if(term.equals(s)) return true;
+    private static int indexOf(String[] L,String term){
+      for(int i=0;i<L.length;i++){
+        if(term.equals(L[i])) return i;
       }
-      return false;
+      return -1;
     }
 }
